3325db3084a6e7a549f6bbf2f1097a2bbe8663d3
Clint Dev <cmaruti93@gmail.com>
2019-07-18 10:44:08 +0300

    Database Setup

--- app.js
+++ app.js
@@ -5,6 +5,18 @@ const bodyParser = require('body-parser');
 require('dotenv/config');
 
 
+// Test route
+app.get('/', (req,res) => {
+    res.send('Hey there!!!');
+});
+
+// Connect to Mongodb
+mongoose.connect(
+    process.env.DB_CONNECTION,
+    { useNewUrlParser: true },
+    () => console.log('Connected to the database successfully!')
+);
+
 // Listen to a http port
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port} ...`));
